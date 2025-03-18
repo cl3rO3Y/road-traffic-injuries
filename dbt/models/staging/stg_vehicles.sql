d9bb1389c170a93570725b6d83b3f99caee9d1b7
Claire ROUBEY <claire.roubey@gmail.com>
2025-03-18 16:24:14 +0000

    fix casting id_user and id_vehicle

--- dbt/models/staging/stg_vehicles.sql
+++ dbt/models/staging/stg_vehicles.sql
@@ -8,7 +8,10 @@ with stg_vehicles as
 
 select
     {{ dbt.safe_cast("Num_Acc", api.Column.translate_type("string")) }} as Num_Acc,
-    {{ dbt.safe_cast("id_vehicule", api.Column.translate_type("integer")) }} as id_vehicule,
+    {{ dbt.safe_cast(
+        "REPLACE(REPLACE(id_vehicule, '\\u00a0', ''), ' ', '')", 
+        api.Column.translate_type("integer")
+        ) }} as id_vehicule,
     num_veh,
     {{ get_engine_type("motor") }} as engine_description,
     {{ get_vehicle_category("catv") }} as vehicle_category
